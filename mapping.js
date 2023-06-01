ccff13dbc61ec18adc771328e9c549357622a160
sourabh <sourabhpande43@gmail.comm>
2023-06-01 17:18:41 +0530

    firstcommit:analysis & study docs

---
+++ mapping.js
@@ -0,0 +1,27 @@
+/**
+ ----------------NEW-----------------
+
+ ----------------HEADING-------------
+ @💹TITLE:Cloudnary & EJS
+
+ @📝Handling forms nd Images.
+
+ @😶‍🌫️GONNA_FOCUS:->A little fronted is going to introduced in this section,once after finished this then just only focus on "Backend"';
+
+ @💹😗FOCUS: Bootstrap,EJS,Cloudnary
+ -creating some form by bear minimum use of "Bootstrap",
+
+ @🔺FIRSTLY:
+ Need to FOCUS on "EJS"
+ @🧐why?? => you can also inject Bootstrap, plane HTML css & Javascript, then why "EJS":
+ =>
+ 🎗️let's i can also inject those code like Bootstrap and plane HTML & Js here so thin I have to explicitely prepared my Backend here then I've to call fronted via Js,then I've to introdued "Cors",then again I've to introduced fetch request & whole bunch of things.i really want to do that.so for that some of "💹termplete enging" come into picture. that's is 
+
+ -🔺EJS
+  Ref:=> 🔗✈️ https://ejs.co/
+  -🎗️By, the term termplete mean put HTML kind of code into "Node" environment kind of application.
+  -🎗️EJS it use for full fladge web application in the mid case or some of the scalar "node" application Fact: no fronted itself.
+  -🎗️Given the fact we'wont be using most of the industrey not preffer this approach this days.
+  People preffer: API driven approach is more familier things so, need to be consume on mobile, as well as web so this one we use.
+
+ */
