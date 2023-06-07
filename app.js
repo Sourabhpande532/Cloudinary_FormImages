47faf3ecd8ef185aad26cd705f57e247527944d9
sourabh <sourabhpande43@gmail.comm>
2023-06-07 17:08:18 +0530

    EJS - Study about Templetes engine and apply

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.listen(5000,()=>console.log("Server is on port 500"))
+app.set("view engine", "ejs");
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/myapp", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+/* @IDENTIFIRE:😇 ↖️*/
+
+app.get("/getform", (req, res) => {
+  res.render("getform");
+});
+
+app.get("/postform", (req, res) => {
+  res.render("postform");
+});
+
+/*
+ @IDENTIFIRE👉👉TITLE: TEMPLATE ENGINE🔝🔝
+*/
+
+const PORT = 5000;
+app.listen(PORT, () =>
+  console.log(`The server is on http://localhost:${PORT}/postform`)
+);
